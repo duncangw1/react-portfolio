28d007b2e8e17ada80fe5e89b854f4e9124b90ff
WD40 <69653174+duncangw1@users.noreply.github.com>
2021-02-04 13:54:53 -0500

    Updated Contact section to remove form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -22,95 +22,23 @@ class Contact extends Component {
 
           <div className="ten columns">
             <p className="lead">{message}</p>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="eight columns">
-            <form action="" method="post" id="contactForm" name="contactForm">
-              <fieldset>
-                <div>
-                  <label htmlFor="contactName">
-                    Name <span className="required">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    defaultValue=""
-                    size="35"
-                    id="contactName"
-                    name="contactName"
-                    onChange={this.handleChange}
-                  />
+            <div className="row">
+              <aside className="four columns footer-widgets">
+                <div className="widget widget_contact">
+                  <h4>Address, Phone, and Email</h4>
+                  <p className="address">
+                    {name}
+                    <br />
+                    {city}, {state}
+                    <br />
+                    <span>{phone}</span>
+                    <br />
+                    <span>{email}</span>
+                  </p>
                 </div>
-
-                <div>
-                  <label htmlFor="contactEmail">
-                    Email <span className="required">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    defaultValue=""
-                    size="35"
-                    id="contactEmail"
-                    name="contactEmail"
-                    onChange={this.handleChange}
-                  />
-                </div>
-
-                <div>
-                  <label htmlFor="contactSubject">Subject</label>
-                  <input
-                    type="text"
-                    defaultValue=""
-                    size="35"
-                    id="contactSubject"
-                    name="contactSubject"
-                    onChange={this.handleChange}
-                  />
-                </div>
-
-                <div>
-                  <label htmlFor="contactMessage">
-                    Message <span className="required">*</span>
-                  </label>
-                  <textarea
-                    cols="50"
-                    rows="15"
-                    id="contactMessage"
-                    name="contactMessage"
-                  ></textarea>
-                </div>
-
-                <div>
-                  <button className="submit">Submit</button>
-                  <span id="image-loader">
-                    <img alt="" src="images/loader.gif" />
-                  </span>
-                </div>
-              </fieldset>
-            </form>
-
-            <div id="message-warning"> Error</div>
-            <div id="message-success">
-              <i className="fa fa-check"></i>Your message was sent, thank you!
-              <br />
+              </aside>
             </div>
           </div>
-
-          <aside className="four columns footer-widgets">
-            <div className="widget widget_contact">
-              <h4>Address and Phone</h4>
-              <p className="address">
-                {name}
-                <br />
-                {city}, {state}
-                <br />
-                <span>{phone}</span>
-                <br />
-                <span>{email}</span>
-              </p>
-            </div>
-          </aside>
         </div>
       </section>
     );
