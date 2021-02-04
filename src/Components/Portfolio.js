1abaa759f51fee091b9d106270a4801bb157baed
WD40 <69653174+duncangw1@users.noreply.github.com>
2021-02-04 13:25:35 -0500

    Updated portfolio with six works and added a button with a link to each github repo

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Portfolio extends Component {
   render() {
-
     if (this.props.data) {
       var projects = this.props.data.projects.map(function (projects) {
-        var projectImage = 'images/portfolio/' + projects.image;
-        
+        var projectImage = "images/portfolio/" + projects.image;
+
         return (
           <div key={projects.title} className="columns portfolio-item">
             <div className="item-wrap">
@@ -16,25 +15,37 @@ class Portfolio extends Component {
                   <div className="portfolio-item-meta">
                     <h5>{projects.title}</h5>
                     <p>{projects.category}</p>
+                    <button
+                      className="githubBtn"
+                      type="button"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        const href = projects.github;
+                        window.location.href = href;
+                      }}
+                    >
+                      {" "}
+                      Github Repo
+                    </button>
                   </div>
                 </div>
               </a>
             </div>
           </div>
-        )
-      })
+        );
+      });
     }
 
     return (
       <section id="portfolio">
-
         <div className="row">
-
           <div className="twelve columns collapsed">
+            <h1>Check Out Some of My Works (click image for Demo)</h1>
 
-            <h1>Check Out Some of My Works.</h1>
-
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
               {projects}
             </div>
           </div>
